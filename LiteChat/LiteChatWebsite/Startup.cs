7f5414bc532c255bacac84de946ebc773d632ffd
MDevoldere <mdevoldere@users.noreply.github.com>
2021-01-11 15:40:17 +0100

    Update Startup.cs
    On peut activer le routage.
    + Ajout IConfiguration

--- LiteChat/LiteChatWebsite/Startup.cs
+++ LiteChat/LiteChatWebsite/Startup.cs
@@ -8,6 +8,14 @@ namespace LiteChatWebsite
 {
     public class Startup
     {
+        // sera necessaire pour referencer le contexte de base de données
+        public IConfiguration Configuration { get; }
+        
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -23,24 +31,17 @@ namespace LiteChatWebsite
                 app.UseDeveloperExceptionPage();
             }
 
-            //app.UseDefaultFiles();
-            //app.UseStaticFiles();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseRouting();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapControllerRoute(
-            //        name: "default",
-            //        pattern: "{controller=Home}/{action=Index}/{id?}");
-            //});
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
         }
     }
 }
