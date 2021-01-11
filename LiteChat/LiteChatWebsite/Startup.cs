8aa8b21988c41a9b7b8bb0e67bd7051edf105018
MDevoldere <mdevoldere@users.noreply.github.com>
2021-01-11 15:42:20 +0100

    Update Startup.cs

--- LiteChat/LiteChatWebsite/Startup.cs
+++ LiteChat/LiteChatWebsite/Startup.cs
@@ -1,8 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MVC1.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace LiteChatWebsite
 {
