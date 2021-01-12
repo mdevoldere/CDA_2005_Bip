03d2079587a183aeddc52afe3480babae526c981
kyweez <auboudier.pro@gmail.com>
2021-01-12 09:46:35 +0100

    kyweez/UserModel

--- LiteChat/LiteChatWebsite/Models/UserModel.cs
+++ LiteChat/LiteChatWebsite/Models/UserModel.cs
@@ -1,6 +1,32 @@
-﻿namespace LiteChatWebsite.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LiteChatWebsite.Models
 {
+    [Table("t_users")]
     public class UserModel
     {
+        [Key]
+        [Column("user_id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID
+        {
+            get; set;
+        }
+
+        [Column("user_name")]
+        [MaxLength(50)]
+        [Required]
+        public string UserName
+        {
+            get; set;
+        }
+
+        [Column("user_password", TypeName = "char(72)")]
+        [Required]
+        public string UserPassWord
+        {
+            get; set;
+        }
     }
 }
