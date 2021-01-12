0853d0fa4bd728093afafa047e8f26a26c25d940
Nthouvenot <58287085+Nthouvenot@users.noreply.github.com>
2021-01-12 10:12:14 +0100

    nthouvenot/RoleModel

---
+++ LiteChat/LiteChatWebsite/Models/RoleModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiteChatWebsite.Models
+{
+    [Table("t_roles")]
+    public class RoleModel
+    {
+        [Key]
+        [Column("role_id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Column("role_type")]
+        [MaxLength(20)]
+        [Required]
+        public string RoleType { get; set; }
+    }
+}
