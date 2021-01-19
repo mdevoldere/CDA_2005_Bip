b41cd2ae2f53f0e4004f3b29e682907626945718
kyweez <auboudier.pro@gmail.com>
2021-01-19 11:21:23 +0100

    kyweez/ j'en ai marre

--- LiteChat/LiteChatWebsite/Startup.cs
+++ LiteChat/LiteChatWebsite/Startup.cs
@@ -1,30 +1,36 @@
-using LiteChatWebsite.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+//using MVC1.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace LiteChatWebsite
 {
     public class Startup
     {
         // sera necessaire pour referencer le contexte de base de données
-        public IConfiguration Configuration { get; }
-        
+        public IConfiguration Configuration
+        {
+            get;
+        }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-        
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddDbContext<LiteChatContext>(options => options.UseSqlServer(Configuration.GetConnectionString("LiteChatConnectionStrings")));
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
